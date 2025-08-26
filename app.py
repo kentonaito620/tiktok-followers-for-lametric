560b193af606313ef6550d90b30e4af1394ef4ef
Kento <kentonaito620@gmail.com>
2025-08-25 22:54:09 -0700

    Fix HEAD response for LaMetric validation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response
 
 app = Flask(__name__)
 
@@ -10,10 +10,13 @@ def serve_followers():
             {"text": "Followers: 4603", "icon": None}
         ]
     }
+
+    if request.method == 'HEAD':
+        # Return headers only, no body
+        return Response(status=200, mimetype='application/json')
+
     return jsonify(data)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-#yessir

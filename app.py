6aa652d12530295f82c6ad384d71d5188f230951
Kento <kentonaito620@gmail.com>
2025-08-25 22:43:11 -0700

    Serve LaMetric-compatible JSON via Flask

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, send_file
+import os
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def serve_json():
-    return send_file('followers.json', mimetype='application/json')
+@app.route('/', methods=['GET', 'HEAD'])
+def serve_followers():
+    data = {
+        "frames": [
+            {"text": "Followers: 4603", "icon": None}
+        ]
+    }
+    return jsonify(data)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
